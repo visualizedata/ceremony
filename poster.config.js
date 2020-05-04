8a4b128d4d903a7a3af9934f8db7f9befa9f406b
Daniel Sauter <daniel@ket.ai>
2020-05-04 11:46:59 -0400

    initial commit

---
+++ poster.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'CEREMONY',
+  department: 'MS Data Visualization',
+  school: 'Parsons School of Design',
+  location: 'zoom',
+  date: '2020-05-13',
+  time: '11:30AM',
+  students: [
+    'Mio Akasako',
+    'Amanda Anderson-You',
+    'Marisa Asari',
+    'Zui Chen',
+    'Robin Coenen',
+    'Michael Desai',
+    'Antonie Dreyer',
+    'Daniel Grunebaum',
+    'Xingwei Huang',
+    'Brad MacDonald',
+    'Yiran Ni',
+    'Neil Oliver',
+    'John Outwater',
+    'Caitlyn Ralph',
+    'Dan Ran',
+    'Saloni Naishad Shah',
+    'Lulu Tanenbaum',
+    'Janice Yamanaka-Lew',
+    'Nour Zein'
+  ]
+}
